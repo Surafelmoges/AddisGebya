a04abf4f4fe1753fa1e8221c8b8f730a3d807e9c
Surafel Moges <surafelmoges01@gmail.com>
2025-01-17 08:39:35 +0000

    Save the data,Generate HTML and make ineractive the page

---
+++ data/products.js
@@ -0,0 +1,44 @@
+export const products = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/New/images/products/comp1.jpg",
+    name: "HP Laptop",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090,
+    keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images/New/images/products/comp2.jpg",
+    name: "Iphone14",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images/New/images/products/comp3.jpg",
+    name: "PS5 Playstation",
+    rating: {
+      stars: 4,
+      count: 127
+    },
+    priceCents: 2095,
+    keywords: [
+      "sports",
+      "basketballs"
+    ]
+  },];
