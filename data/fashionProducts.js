8910996301fcdd428b5b3591d316a0b9f174daaf
Surafel Moges <surafelmoges01@gmail.com>
2025-01-30 08:54:43 +0000

    Add more products on shop deals fashions

---
+++ data/fashionProducts.js
@@ -0,0 +1,133 @@
+export const fashionPro =[{
+    
+  id: "bc2847e9-5323-403f-b7cf-57fde044a955A",
+  image: "images/fashion/b.jpg",
+  Name: "Men's Fleece Shoes",
+  rating: {
+    stars: 4.5,
+    count: 3157
+  },
+  priceCents: 2400,
+keywords: [
+  "sweaters",
+  "hoodies",
+  "apparel",
+  "mens"
+]
+},
+{
+  id: "15b6fc6f-327a-4ec4-896f-486349e85a3dB",
+  image: "images/fashion/c.jpg",
+  Name: "Men hoodie",
+  rating:{
+    stars:4.5,
+    count:4556
+  },
+  priceCents: 7400,
+  keywords: [
+    "socks",
+    "sports",
+    "apparel"
+  ]
+
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3dC",
+    image: "images/fashion/d.jpg",
+    Name: "Xoodie",
+    rating:{
+      stars:4.5,
+      count:6556
+    },
+    priceCents: 740,
+    keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  
+    },
+    {
+      id: "15b6fc6f-327a-4ec4-896f-486349e85a3dC",
+      image: "images/fashion/e.jpg",
+      Name: "Xoodie",
+      rating:{
+        stars:4.5,
+        count:6556
+      },
+      priceCents: 740,
+      keywords: [
+        "socks",
+        "sports",
+        "apparel"
+      ]
+    
+      },
+      {
+        id: "15b6fc6f-327a-4ec4-896f-486349e85a3dC",
+        image: "images/fashion/f.jpg",
+        Name: "Xoodie",
+        rating:{
+          stars:4.5,
+          count:6556
+        },
+        priceCents: 740,
+        keywords: [
+          "socks",
+          "sports",
+          "apparel"
+        ]
+      
+        },
+        {
+          id: "15b6fc6f-327a-4ec4-896f-486349e85a3dC",
+          image: "images/fashion/g.jpg",
+          Name: "Xoodie",
+          rating:{
+            stars:4.5,
+            count:6556
+          },
+          priceCents: 740,
+          keywords: [
+            "socks",
+            "sports",
+            "apparel"
+          ]
+        
+          },
+          {
+            id: "15b6fc6f-327a-4ec4-896f-486349e85a3dC",
+            image: "images/fashion/h.jpg",
+            Name: "Xoodie",
+            rating:{
+              stars:4.5,
+              count:6556
+            },
+            priceCents: 740,
+            keywords: [
+              "socks",
+              "sports",
+              "apparel"
+            ]
+          
+            },
+            {
+              id: "15b6fc6f-327a-4ec4-896f-486349e85a3dC",
+              image: "images/fashion/i.jpg",
+              Name: "Xoodie",
+              rating:{
+                stars:4.5,
+                count:6556
+              },
+              priceCents: 740,
+              keywords: [
+                "socks",
+                "sports",
+                "apparel"
+              ]
+            
+              }
+    
+];
+
+
